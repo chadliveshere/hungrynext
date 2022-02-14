d51af9f71918ba439addafeab786e6fb07970120
Chad Everett <c_crabtree@mac.com>
2022-02-14 00:48:10 -0600

    escape double quotes

--- components/Footer.js
+++ components/Footer.js
@@ -40,7 +40,7 @@ export default function Footer() {
 			</div>
 			<div className={styles.footerDisclaimer}>
 				<p>
-					The information provided by Hungry Neighbors ("we," "us", or "our") on
+					The information provided by Hungry Neighbors (&ldquo;we,&ldquo; &ldquo;us&ldquo;, or &ldquo;our&ldquo;) on
 					<a href='http://www.hungryneighbors.com'>http://www.hungryneighbors.com</a>
 					is for general informational purposes only. All information on the Site is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability or completeness of any information on the Site. Under no circumstance shall we have any liability to you for any loss or damage of any kind incurred as a result of the use of the site or reliance on any information provided on the site. Your
 					use of the site and your reliance on any information on the site is solely at your own risk.
